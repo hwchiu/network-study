47d648dd2d924505a3ed49b16a0e934d60443184
Hung-Wei Chiu <hungwei@opennetworking.org>
2020-04-09 23:57:11 -0700

    Add iperf server

--- iptables/iptables_ebtables/setup_docker.sh
+++ iptables/iptables_ebtables/setup_docker.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 docker rm -f nginx
 docker rm -f netutils
+docker rm -f iperf
 
 docker run -d -p 5566:80 --name nginx nginx
 docker run -d --name netutils hwchiu/netutils
+docker run -d --name iperf -p 12345:5201 --entrypoint iperf3 hwchiu/netutils -s -p 5201
