dcdf01397069ad657f8bcab28237ee9a18c40c59
Hung-Wei Chiu <hungwei@opennetworking.org>
2020-04-10 22:06:41 -0700

    Remove duplicate rules

--- iptables/iptables_ebtables/iptables.sh
+++ iptables/iptables_ebtables/iptables.sh
@@ -1,71 +1,44 @@
 #!/bin/bash
 
 insert() {
-    iptables -t raw -I PREROUTING -p icmp -d 172.17.0.0/16 -j LOG --log-prefix 'iptable/raw-PREROUTE' --log-level debug
-    iptables -t raw -I PREROUTING -p icmp  -d 172.18.0.0/16 -j LOG --log-prefix 'iptable/raw-PREROUTE' --log-level debug
-    iptables -t mangle -I PREROUTING -p icmp -d 172.18.0.0/16 -j LOG --log-prefix 'iptable/mangle-PREROUTE' --log-level debug
-    iptables -t mangle -I PREROUTING -p icmp -d 172.17.0.0/16 -j LOG --log-prefix 'iptable/mangle-PREROUTE' --log-level debug
-    iptables -t nat -I PREROUTING -p icmp  -d 172.18.0.0/16 -j LOG --log-prefix 'iptable/nat-PREROUTE' --log-level debug
-    iptables -t nat -I PREROUTING -p icmp  -d 172.17.0.0/16 -j LOG --log-prefix 'iptable/nat-PREROUTE' --log-level debug
+    iptables -t raw -I PREROUTING -p icmp -j LOG --log-prefix 'iptable/raw-PREROUTE' --log-level debug
+    iptables -t mangle -I PREROUTING -p icmp -j LOG --log-prefix 'iptable/mangle-PREROUTE' --log-level debug
+    iptables -t nat -I PREROUTING -p icmp -j LOG --log-prefix 'iptable/nat-PREROUTE' --log-level debug
 
-    iptables -t mangle -I FORWARD -p icmp -d 172.18.0.0/16 -j LOG --log-prefix 'iptable/mangle-FORWARD' --log-level debug
-    iptables -t mangle -I FORWARD -p icmp -d 172.17.0.0/16 -j LOG --log-prefix 'iptable/mangle-FORWARD' --log-level debug
-    iptables -t filter -I FORWARD -p icmp -d 172.18.0.0/16 -j LOG --log-prefix 'iptable/filter-FORWARD' --log-level debug
-    iptables -t filter -I FORWARD -p icmp -d 172.17.0.0/16 -j LOG --log-prefix 'iptable/filter-FORWARD' --log-level debug
+    iptables -t mangle -I FORWARD -p icmp -j LOG --log-prefix 'iptable/mangle-FORWARD' --log-level debug
+    iptables -t filter -I FORWARD -p icmp -j LOG --log-prefix 'iptable/filter-FORWARD' --log-level debug
 
-    iptables -t mangle -I INPUT -p icmp -d 172.18.0.0/16 -j LOG --log-prefix 'iptable/mangle-INPUT' --log-level debug
-    iptables -t mangle -I INPUT -p icmp -d 172.17.0.0/16 -j LOG --log-prefix 'iptable/mangle-INPUT' --log-level debug
-    iptables -t filter -I INPUT -p icmp -d 172.18.0.0/16 -j LOG --log-prefix 'iptable/filter-INPUT' --log-level debug
-    iptables -t filter -I INPUT -p icmp -d 172.17.0.0/16 -j LOG --log-prefix 'iptable/filter-INPUT' --log-level debug
+    iptables -t mangle -I INPUT -p icmp -j LOG --log-prefix 'iptable/mangle-INPUT' --log-level debug
+    iptables -t filter -I INPUT -p icmp -j LOG --log-prefix 'iptable/filter-INPUT' --log-level debug
 
 
-    iptables -t raw -I OUTPUT -p icmp -d 172.17.0.0/16 -j LOG --log-prefix 'iptable/raw-OUTPUT' --log-level debug
-    iptables -t raw -I OUTPUT -p icmp  -d 172.18.0.0/16 -j LOG --log-prefix 'iptable/raw-OUTPUT' --log-level debug
-    iptables -t mangle -I OUTPUT -p icmp -d 172.18.0.0/16 -j LOG --log-prefix 'iptable/mangle-OUTPUT' --log-level debug
-    iptables -t mangle -I OUTPUT -p icmp -d 172.17.0.0/16 -j LOG --log-prefix 'iptable/mangle-OUTPUT' --log-level debug
-    iptables -t nat -I OUTPUT -p icmp  -d 172.18.0.0/16 -j LOG --log-prefix 'iptable/nat-OUTPUT' --log-level debug
-    iptables -t nat -I OUTPUT -p icmp  -d 172.17.0.0/16 -j LOG --log-prefix 'iptable/nat-OUTPUT' --log-level debug
-    iptables -t filter -I OUTPUT -p icmp -d 172.18.0.0/16 -j LOG --log-prefix 'iptable/filter-OUTPUT' --log-level debug
-    iptables -t filter -I OUTPUT -p icmp -d 172.17.0.0/16 -j LOG --log-prefix 'iptable/filter-OUTPUT' --log-level debug
+    iptables -t raw -I OUTPUT -p icmp -j LOG --log-prefix 'iptable/raw-OUTPUT' --log-level debug
+    iptables -t mangle -I OUTPUT -p icmp -j LOG --log-prefix 'iptable/mangle-OUTPUT' --log-level debug
+    iptables -t nat -I OUTPUT -p icmp -j LOG --log-prefix 'iptable/nat-OUTPUT' --log-level debug
+    iptables -t filter -I OUTPUT -p icmp -j LOG --log-prefix 'iptable/filter-OUTPUT' --log-level debug
 
-    iptables -t mangle -I POSTROUTING -p icmp -d 172.18.0.0/16 -j LOG --log-prefix 'iptable/mangle-POSTROUTE' --log-level debug
-    iptables -t mangle -I POSTROUTING -p icmp -d 172.17.0.0/16 -j LOG --log-prefix 'iptable/mangle-POSTROUTE' --log-level debug
-    iptables -t nat -I POSTROUTING -p icmp -d 172.18.0.0/16 -j LOG --log-prefix 'iptable/nat-POSTROUTE' --log-level debug
-    iptables -t nat -I POSTROUTING -p icmp -d 172.17.0.0/16 -j LOG --log-prefix 'iptable/nat-POSTROUTE' --log-level debug
+    iptables -t mangle -I POSTROUTING -p icmp -j LOG --log-prefix 'iptable/mangle-POSTROUTE' --log-level debug
+    iptables -t nat -I POSTROUTING -p icmp -j LOG --log-prefix 'iptable/nat-POSTROUTE' --log-level debug
  }
 
 delete() {
-        iptables -t raw -D PREROUTING -p icmp -d 172.17.0.0/16 -j LOG --log-prefix 'iptable/raw-PREROUTE' --log-level debug
-    iptables -t raw -D PREROUTING -p icmp  -d 172.18.0.0/16 -j LOG --log-prefix 'iptable/raw-PREROUTE' --log-level debug
-    iptables -t mangle -D PREROUTING -p icmp -d 172.18.0.0/16 -j LOG --log-prefix 'iptable/mangle-PREROUTE' --log-level debug
-    iptables -t mangle -D PREROUTING -p icmp -d 172.17.0.0/16 -j LOG --log-prefix 'iptable/mangle-PREROUTE' --log-level debug
-    iptables -t nat -D PREROUTING -p icmp  -d 172.18.0.0/16 -j LOG --log-prefix 'iptable/nat-PREROUTE' --log-level debug
-    iptables -t nat -D PREROUTING -p icmp  -d 172.17.0.0/16 -j LOG --log-prefix 'iptable/nat-PREROUTE' --log-level debug
+    iptables -t raw -D PREROUTING -p icmp -j LOG --log-prefix 'iptable/raw-PREROUTE' --log-level debug
+    iptables -t mangle -D PREROUTING -p icmp -j LOG --log-prefix 'iptable/mangle-PREROUTE' --log-level debug
+    iptables -t nat -D PREROUTING -p icmp -j LOG --log-prefix 'iptable/nat-PREROUTE' --log-level debug
 
-    iptables -t mangle -D FORWARD -p icmp -d 172.18.0.0/16 -j LOG --log-prefix 'iptable/mangle-FORWARD' --log-level debug
-    iptables -t mangle -D FORWARD -p icmp -d 172.17.0.0/16 -j LOG --log-prefix 'iptable/mangle-FORWARD' --log-level debug
-    iptables -t filter -D FORWARD -p icmp -d 172.18.0.0/16 -j LOG --log-prefix 'iptable/filter-FORWARD' --log-level debug
-    iptables -t filter -D FORWARD -p icmp -d 172.17.0.0/16 -j LOG --log-prefix 'iptable/filter-FORWARD' --log-level debug
+    iptables -t mangle -D FORWARD -p icmp -j LOG --log-prefix 'iptable/mangle-FORWARD' --log-level debug
+    iptables -t filter -D FORWARD -p icmp -j LOG --log-prefix 'iptable/filter-FORWARD' --log-level debug
 
-    iptables -t mangle -D INPUT -p icmp -d 172.18.0.0/16 -j LOG --log-prefix 'iptable/mangle-INPUT' --log-level debug
-    iptables -t mangle -D INPUT -p icmp -d 172.17.0.0/16 -j LOG --log-prefix 'iptable/mangle-INPUT' --log-level debug
-    iptables -t filter -D INPUT -p icmp -d 172.18.0.0/16 -j LOG --log-prefix 'iptable/filter-INPUT' --log-level debug
-    iptables -t filter -D INPUT -p icmp -d 172.17.0.0/16 -j LOG --log-prefix 'iptable/filter-INPUT' --log-level debug
+    iptables -t mangle -D INPUT -p icmp -j LOG --log-prefix 'iptable/mangle-INPUT' --log-level debug
+    iptables -t filter -D INPUT -p icmp -j LOG --log-prefix 'iptable/filter-INPUT' --log-level debug
 
-    iptables -t raw -D OUTPUT -p icmp -d 172.17.0.0/16 -j LOG --log-prefix 'iptable/raw-OUTPUT' --log-level debug
-    iptables -t raw -D OUTPUT -p icmp  -d 172.18.0.0/16 -j LOG --log-prefix 'iptable/raw-OUTPUT' --log-level debug
-    iptables -t mangle -D OUTPUT -p icmp -d 172.18.0.0/16 -j LOG --log-prefix 'iptable/mangle-OUTPUT' --log-level debug
-    iptables -t mangle -D OUTPUT -p icmp -d 172.17.0.0/16 -j LOG --log-prefix 'iptable/mangle-OUTPUT' --log-level debug
-    iptables -t nat -D OUTPUT -p icmp  -d 172.18.0.0/16 -j LOG --log-prefix 'iptable/nat-OUTPUT' --log-level debug
-    iptables -t nat -D OUTPUT -p icmp  -d 172.17.0.0/16 -j LOG --log-prefix 'iptable/nat-OUTPUT' --log-level debug
-    iptables -t filter -D OUTPUT -p icmp -d 172.18.0.0/16 -j LOG --log-prefix 'iptable/filter-OUTPUT' --log-level debug
-    iptables -t filter -D OUTPUT -p icmp -d 172.17.0.0/16 -j LOG --log-prefix 'iptable/filter-OUTPUT' --log-level debug
+    iptables -t raw -D OUTPUT -p icmp -j LOG --log-prefix 'iptable/raw-OUTPUT' --log-level debug
+    iptables -t mangle -D OUTPUT -p icmp -j LOG --log-prefix 'iptable/mangle-OUTPUT' --log-level debug
+    iptables -t nat -D OUTPUT -p icmp -j LOG --log-prefix 'iptable/nat-OUTPUT' --log-level debug
+    iptables -t filter -D OUTPUT -p icmp -j LOG --log-prefix 'iptable/filter-OUTPUT' --log-level debug
 
-    iptables -t mangle -D POSTROUTING -p icmp -d 172.18.0.0/16 -j LOG --log-prefix 'iptable/mangle-POSTROUTE' --log-level debug
-    iptables -t mangle -D POSTROUTING -p icmp -d 172.17.0.0/16 -j LOG --log-prefix 'iptable/mangle-POSTROUTE' --log-level debug
-    iptables -t nat -D POSTROUTING -p icmp -d 172.18.0.0/16 -j LOG --log-prefix 'iptable/nat-POSTROUTE' --log-level debug
-    iptables -t nat -D POSTROUTING -p icmp -d 172.17.0.0/16 -j LOG --log-prefix 'iptable/nat-POSTROUTE' --log-level debug
-
+    iptables -t mangle -D POSTROUTING -p icmp -j LOG --log-prefix 'iptable/mangle-POSTROUTE' --log-level debug
+    iptables -t nat -D POSTROUTING -p icmp -j LOG --log-prefix 'iptable/nat-POSTROUTE' --log-level debug
 }
 
 check() {
